58fd18911dcaae0bd57829e4d7f7f9f7351d70f2
Thomas Huth <huth@users.berlios.de>
2009-06-14 13:05:58 +0200

    Added Super Sprint.

---
+++ src/includes/debugui.h
@@ -0,0 +1,23 @@
+/*
+  Hatari - debugui.h
+
+  This file is distributed under the GNU Public License, version 2 or at
+  your option any later version. Read the file gpl.txt for details.
+*/
+
+#ifndef HATARI_DEBUGUI_H
+#define HATARI_DEBUGUI_H
+
+/* DebugUI_ParseCommand() return values */
+enum {
+	DEBUGGER_END,      // Leave debugger
+	DEBUGGER_CMDCONT,  // Command can continue
+	DEBUGGER_CMDDONE   // Command done
+};
+
+extern void DebugUI(void);
+extern int DebugUI_ParseCommand(char *input);
+extern void DebugUI_CpuCheck(void);
+extern void DebugUI_DspCheck(void);
+
+#endif /* HATARI_DEBUGUI_H */
